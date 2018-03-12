fefb7c907bfb38043335c65dc8d5d5b81e15105f
wenning <wenning@bigbaser.com>
2018-03-12 14:33:16 +0800

    add more examples

--- example/client.py
+++ example/client.py
@@ -9,8 +9,30 @@ from protos import hello_pb2_grpc
 def run():
     channel = grpc.insecure_channel('localhost:9094')
     stub = hello_pb2_grpc.GreeterStub(channel)
+    # simple
     response = stub.SayHello(hello_pb2.HelloRequest(name='you'))
+    print("simple rpc: \n ==============\n")
     print("Greeter client received: " + response.message)
+    # stream response
+    print("response streaming rpc: \n ==============\n")
+    responses = stub.ListNames(hello_pb2.HelloRequest(name='Peter,Harry,Mike'))
+    for res in responses:
+        print(res)
+    # steam request
+    print("request streaming rpc: \n ==============\n")
+
+    def get_names():
+        for i in ['name1', 'name2', 'name3', 'name4']:
+            yield hello_pb2.HelloRequest(name=i)
+
+    response = stub.HelloMany(get_names())
+    print(response)
+
+    # bidirectional
+    print("bidirectional streaming rpc: \n ==============\n")
+    responses = stub.HelloChat(get_names())
+    for res in responses:
+        print(res)
 
 
 if __name__ == '__main__':
--- example/server.py
+++ example/server.py
@@ -1,8 +1,8 @@
 from python_kit import PythonKit
 
-
 server = PythonKit(__name__, protos='./protos/hello.proto')
 servicer = server.create_servicer('Greeter')
+
 
 @servicer.endpoint('SayHello')
 def SayHello(self, request, context):
@@ -10,6 +10,32 @@ def SayHello(self, request, context):
     return servicer.response('HelloReply', {
         'message': hello_to
     })
+
+
+@servicer.endpoint('ListNames')
+def ListNames(self, request, context):
+    for name in request.name.split(','):
+        yield servicer.response('HelloReply', {
+            'message': name
+        })
+
+
+@servicer.endpoint('HelloMany')
+def HelloMany(self, request, context):
+    names = ""
+    for single_request in request:
+        names += single_request.name
+    return servicer.response('HelloReply', {
+        'message': 'Hello: {}'.format(names)
+    })
+
+
+@servicer.endpoint('HelloChat')
+def HelloChat(self, request, context):
+    for single_reqeust in request:
+        yield servicer.response('HelloReply', {
+            'message': 'Hello: {}'.format(single_reqeust.name)
+        })
 
 
 if __name__ == "__main__":
